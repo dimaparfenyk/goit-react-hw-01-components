509fcfbc2e01725af46fc0bbaefe487054e007d4
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2022-07-03 19:51:20 +0300

    add reusable var

--- src/components/FriendsList/FriendListItem.jsx
+++ src/components/FriendsList/FriendListItem.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
-import css from './friend.module.css'
+import css from './friend.module.css';
+import { iconSize } from 'constance';
 
 const FriendListItem = ({ avatar, name, isOnline}) => {
    
   return (<li className={css.item}>
         <span className={css[isOnline]}></span>
-        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+        <img className={css.avatar} src={avatar} alt="User avatar" width={iconSize.lg} />
         <p className={css.name}>{name}</p>
     </li>
     );
